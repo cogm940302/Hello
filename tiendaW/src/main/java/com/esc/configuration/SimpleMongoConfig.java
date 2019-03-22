413b567202b9e57860daac993042cdfc130d8ebf
cogm940302 <jmcg.mg51@gmail.com>
2019-03-22 10:30:06 -0600

    Add files via upload

--- tiendaW/src/main/java/com/esc/configuration/SimpleMongoConfig.java
+++ tiendaW/src/main/java/com/esc/configuration/SimpleMongoConfig.java
@@ -21,3 +21,34 @@ public class SimpleMongoConfig {
 	        return new MongoTemplate(mongo(), "tiendadb");
 	    }
 }
+
+
+
+/***
+ * Script Mongo
+> db  // para saber que bd existen
+>use tiendadb // para crear y moverte a esa bd
+>db.product.insertOne({id:0,nombre:"Producto de prueba",precio:0.0})// inserta producto
+>db.product.find() //encuentra todos los productos
+> db.product.find({id:{$eq:0}})  // find specify product
+> db.product.find({id:0}) // same thing
+> db.product.updateOne({id:0},{$set:{descripcion:"Producto De Prueba Des"}}) // modificar un registro
+
+
+Instrucciones de uso de mongo
+1.- Abrir cmd
+2.- cd C:\Program Files\MongoDB\Server\4.0\bin
+3.- escribir y ejecutar mongod
+4.- ejecutar mongo.exe
+
+Nota: si esto no funciona, checa que exista la carpeta c:/data/db
+
+
+https://docs.mongodb.com/manual/crud/#update-operations
+https://docs.mongodb.com/manual/tutorial/query-documents/#read-operations-query-argument
+https://docs.mongodb.com/manual/reference/operator/query/eq/
+https://spring.io/guides/gs/accessing-mongodb-data-rest/
+https://www.baeldung.com/spring-data-mongodb-tutorial
+https://www.baeldung.com/queries-in-spring-data-mongodb
+
+ * **/
